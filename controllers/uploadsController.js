e1f86a07b775741cf9424cbcfe780dbfe5270671
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-09-07 23:08:30 +0700

    Additional checks

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,14 +1,36 @@
 const path = require('path')
 const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors')
 
 const uploadProductImage = async (req, res) => {
   console.log(req.files)
-  let productImage = req.files.image
+
+  // check if file exists
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded')
+  }
+
+  const productImage = req.files.image
+
+  // check format
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image')
+  }
+
+  const maxSize = 1024 * 1024;
+
+  // check size
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError('Please upload image smaller 1MB');
+  }
+
   const imagePath = path.join(
     __dirname,
     '../public/uploads/' + `${productImage.name}`
   )
+
   await productImage.mv(imagePath)
+
   return res
     .status(StatusCodes.OK)
     .json({ image: { src: `/uploads/${productImage.name}` } })
